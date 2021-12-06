2fe538aa993ba4b536142682a44003b5a2e91375
jesseClegg <jclegg@albany.edu>
2021-12-05 22:07:59 -0500

    optimized

--- partThree.rkt
+++ partThree.rkt
@@ -1,42 +1,11 @@
-;Jesse Clegg
+; Jesse Clegg
 
 ; Takes in a list
 ; returns that list sorted by implementing a recursive bubble sort algorithm
 (define (bubbleSort lst)
   ; Pass this list into the outter loop function of a typical bubblesort algorithm
-  ; passses the length of this list to calculate the number of iterations for the inner loop to garauntee a sorted list 
+  ; passses the length of this list to calculate the number of iterations for the inner loop to guarantee a sorted list 
   (outterLoop lst (length lst))
-)
-
-; Function that performs the tasks of the inner loop of bubble sort
-; starts with the first element of a list as the index, compares to each element at n+1 position i.e. cadr of list
-; if n+1 is greater, n+1 and index are swapped
-; else continue walking list by passing cdr of the list into this recursive function
-(define (innerLoop lst)
-  (cond
-       ; List is empty   
-       ((null? lst)
-        (display 5999);we could optimize here
-        '()
-        )
-       ; Case of last item in list
-       ((= 1 (length lst))
-       (cons (car lst) (cdr lst))
-        )
-       ; If first>2nd (yes swap)
-       ((> (car lst) (cadr lst))
-        (cons (cadr lst) (innerLoop (cons (car lst) (cddr lst))))
-        )
-       ; If first<2nd then (no swap), just drop car by passing cdr of the list into innerLoop
-       ((< (car lst) (cadr lst))
-        (cons (car lst) (innerLoop (cdr lst)))
-        )
-       ; Default case, i.e. first and second are =, continue walking the list by passing cdr into innerLoop
-       (else
-        (cons (car lst) (innerLoop (cdr lst)))
-
-  )
- )
 )
 
 ; Function that acts like the outter loop of bubblesort
@@ -44,16 +13,40 @@
 ; length of list dictates inner loop be called n-1 times with n being list size
 ; each functional call decrements n by one, until n=1
 (define (outterLoop lst n)
-  
   (cond
+    ;list has been passed into inner loop n-1 times, guaranteed sorted, return this sorted list
     ((= n 1)
-     (innerLoop lst))
-  ;decrement n until list has been walked n-1 times
+     lst
+     )
   (else
-   (outterLoop (innerLoop lst) (- n 1))
+   (outterLoop (innerLoop lst) (- n 1));decrement n with each function call until list has been walked n-1 times
    )
   )
 )  
+
+; Function that performs the tasks of the inner loop of bubble sort
+; starts with the first element of a list as the index, compares to each element at n+1 position i.e. cadr of each list
+; if n+1 is greater, n+1 and index are swapped
+; else continue walking list by passing cdr of the list into this recursive function 
+(define (innerLoop lst)
+  (cond
+       ; Case of last item in list
+       ; No need to sort a list of one element
+       ((= 1 (length lst))
+       lst
+        )
+       ; If first>second (yes swap)
+       ((> (car lst) (cadr lst))
+        (cons (cadr lst) (innerLoop (cons (car lst) (cddr lst))))
+        )
+       ; Default case, i.e. first and second are =, or first<second, thus no swap
+       ; Continue walking the list by passing cdr into innerLoop
+       ; then rebuild list in order after recursive calls return
+       (else
+        (cons (car lst) (innerLoop (cdr lst)))
+       )
+ )
+)
 
 
 (display (bubbleSort '(10 9 8 7 6 5 4 3 2 1)));proves a list has been returned
